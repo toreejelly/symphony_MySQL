aa8bbcbfa6fac202e9b1285de50ef9a0f460d5b7
toreejelly <toreejelly@gmail.com>
2024-11-05 23:52:52 +0900

    .

--- symphony_MySQL/src/main/java/kr/co/symphony/pay/PayCont.java
+++ symphony_MySQL/src/main/java/kr/co/symphony/pay/PayCont.java
@@ -53,9 +53,9 @@ private PayDAO dao = null;
 		
 		ModelAndView mav = new ModelAndView();
 		
-		//노란줄 생기는 부분 노란줄 없애는 주석.
+		//노란줄 생기는 부분 노란줄 없애는 주석
 		@SuppressWarnings("unchecked")
-		//세션에 담아둔 데이터 가져옴.
+		//세션에 담아둔 데이터 가져옴
 		ArrayList<PayDTO> items = (ArrayList<PayDTO>) session.getAttribute("check");
 		
 		if(items != null) {
